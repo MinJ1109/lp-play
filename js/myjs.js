c86ec5f9f35e4b7343c5e352d4679a95a0b27c54
LeeM <lovesong001109@gmail.com>
2024-05-09 04:45:26 +0900

    discount 제외 전부

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+    // headerScroll();
     navslide();
     searchonoff();
     topslide();
+    wishlistH();
 });
 
 function navslide(){
@@ -18,41 +20,24 @@ function searchonoff(){
     });
 }
 
-// function topslide(){
-//     var widthLi = $('.topSlider > ul li')[0].offsetWidth;
-
-//     $('.sldeBTn1').click(function(){
-//         // $('.topSlider > ul').css('transform', 'translateX(0)');
-//         $('.topSlider > ul li:last-of-type').css('display', 'none');
-//         $('.topSlider > ul li:first-of-type').css('display', 'block');
-//         $(this).addClass('active');
-//         $(this).siblings().removeClass('active');
-//     });
-    
-//     $('.sldeBTn2').click(function(){
-//         // $('.topSlider > ul').css('transform', 'translateX(-' + widthLi + 'px)');
-//         $('.topSlider > ul li:last-of-type').css('display', 'block');
-//         $('.topSlider > ul li:first-of-type').css('display', 'none');
-//         $(this).addClass('active');
-//         $(this).siblings().removeClass('active');
-//     });    
-// }
 function topslide(){
     function nextSlide(){
-        $('.topSlider > ul li:last-of-type').css('display', 'none');
-        $('.topSlider > ul li:first-of-type').css('display', 'block');
-        $('.sldeBTn1').addClass('active');
-        $('.sldeBTn1').siblings().removeClass('active');
+        $('.topSlider > ul').css('transform', 'translateX(0)');
+        $('.sldeBTn1').addClass('active').siblings().removeClass('active');
     }
-
+    
     function prevSlide(){
-       $('.topSlider > ul li:last-of-type').css('display', 'block');
-        $('.topSlider > ul li:first-of-type').css('display', 'none');
-        $('.sldeBTn2').addClass('active');
-        $('.sldeBTn2').siblings().removeClass('active');
-    }
-
+        $('.topSlider > ul').css('transform', 'translateX(-100vw)');
+        $('.sldeBTn2').addClass('active').siblings().removeClass('active');
+    }  
+    
     $('.sldeBTn1').click(nextSlide);
-
     $('.sldeBTn2').click(prevSlide);
 }
+
+function wishlistH(){
+    $("input[value='찜하기']").click(function(){
+        $(this).toggleClass('active');
+    });
+}
+
